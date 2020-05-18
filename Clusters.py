7f4eeafebbad2bdcde88e0f9028b2a310a880238
bnymnsntrk <bnymnsntrk@gmail.com>
2020-05-18 18:40:29 +0300

    Fill missing ones with mean

--- Clusters.py
+++ Clusters.py
@@ -5,6 +5,7 @@ import matplotlib
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
 from sklearn.utils import shuffle
 from sklearn.model_selection import GridSearchCV, StratifiedKFold, train_test_split
 from sklearn.tree import DecisionTreeClassifier
@@ -19,10 +20,18 @@ data = pd.read_csv("data.csv")              #read data
 data = shuffle(data)                        #shuffle data
 data.reset_index(inplace=True, drop=True)   #reset indexes to make shuffle work
 
-x = data[['quarter', 'stock', 'date', 'open', 'high', 'low', 'close', 'volume', 'percent_change_price',
-          'percent_change_volume_over_last_wk', 'previous_weeks_volume', 'next_weeks_open', 'next_weeks_close',
-          'percent_change_next_weeks_price', 'days_to_next_dividend', 'percent_return_next_dividend']]      #features
-
+pd.DataFrame(data).fillna(data.mean)
 #print(data.isna().sum())  # check if there are missing values
 
 data = data.drop(['date'], axis=1)      #we don't need date
+
+labelEncoder = LabelEncoder()           # converting stock to numerical value
+labelEncoder.fit(data['stock'])
+data['stock'] = labelEncoder.transform(data['stock'])
+
+X = np.array(data.drop(['percent_change_next_weeks_price'], 1).astype(float))
+y = np.array(data['percent_change_next_weeks_price'])
+
+
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(X)
