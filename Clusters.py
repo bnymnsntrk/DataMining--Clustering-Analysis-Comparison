7c9bc1dc754d3ade6eea52def2b490ef9167c775
bnymnsntrk <bnymnsntrk@gmail.com>
2020-05-21 15:28:57 +0300

    clean file, 2d data exported

--- Clusters.py
+++ Clusters.py
@@ -1,16 +1,5 @@
-"""Bünyamin Şentürk - 150116007"""
-"""Kemal Barbaros - 150116070"""
-
-import inline as inline
-import matplotlib
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import pandas as pd
-from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import LabelEncoder
-from sklearn.utils import shuffle
-from sklearn.model_selection import GridSearchCV, StratifiedKFold, train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import svm
 import time
 import matplotlib.pyplot as plt
 import numpy as np
@@ -22,41 +11,46 @@ from sklearn.cluster import DBSCAN
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
+"""
+Bünyamin Şentürk - 150116007
+Kemal Barbaros - 150116070
+"""
 
-data = pd.read_csv("data.csv")              #read data
+
+data = pd.read_csv("data.csv")              # read data
 X = data.iloc[:, [8]]
 
 """print(data.isna().sum())  # check if there are missing values"""
-data = data.interpolate(method='linear', limit_direction='forward')
-data = data.drop(['date'], axis=1)      #we don't need date
+data = data.interpolate(method='linear', limit_direction='forward')     # fill missing values with linear forwarding
+data = data.drop(['date'], axis=1)      # we don't need date
 
 labelEncoder = LabelEncoder()           # converting 'stock' to numerical value
 labelEncoder.fit(data['stock'])
 data['stock'] = labelEncoder.transform(data['stock'])
 
-data = data.iloc[1:]            #dropping first row because it can not be interpolated
+data = data.iloc[1:]            # dropping first row because it can not be interpolated
 
-scaler = StandardScaler()                                       #scaling data
+scaler = StandardScaler()                                       # scaling data
 data_scaled = scaler.fit_transform(data)
 
-data_normalized = normalize(data_scaled)                        #normalizing data
+data_normalized = normalize(data_scaled)                        # normalizing data
 data_normalized = pd.DataFrame(data_normalized, columns=data.columns)
 data_normalized.head()
 
-data_normalized.to_csv(r'normalized_data.csv', index=False)     #exporting data to check if it has problems
+# data_normalized.to_csv(r'normalized_data.csv', index=False)     # exporting data to check if it has problems
 
-pca = PCA(n_components=2)
+pca = PCA(n_components=2)                           # reducing it 2 dimensions for better visuality
 data_2d = pca.fit_transform(data_normalized)
 data_2d = pd.DataFrame(data_2d)
 data_2d.columns = ['x', 'y']
 
+# data_2d.to_csv(r'2d.csv', index=False)
 
 def agnes():
     cluster = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='ward')
     cluster.fit_predict(data_2d)
     plt.figure(figsize=(10, 7))
-    plt.scatter(data_2d['x'], data_2d['y'],
-                c=cluster.labels_)
+    plt.scatter(data_2d['x'], data_2d['y'], c=cluster.labels_)
     plt.show()
 
 
@@ -86,8 +80,7 @@ def kMeans():
     kmeans.fit_predict(data_2d)
 
     plt.figure(figsize=(10, 7))
-    plt.scatter(data_2d['x'], data_2d['y'],
-                c=kmeans.labels_)
+    plt.scatter(data_2d['x'], data_2d['y'], c=kmeans.labels_)
     plt.show()
 
     """plt.scatter(X[kmeans == 0, 0], X[kmeans == 0, 1], s=100, c='red', label='Careful')
@@ -102,11 +95,19 @@ def kMeans():
     plt.legend()
     plt.show()"""
 
-#elbow() #ELBOW METHOD FUNCTION, TAKES TOO MUCH TIME TO RUN, ONLY RAN FOR ONCE
-#dendogram() #TO DECIDE CLUSTER NUMBER FOR AGNES, ONLY RAN FOR ONCE
 
+"""
+elbow() #ELBOW METHOD FUNCTION, TAKES TOO MUCH TIME TO RUN, ONLY RAN FOR ONCE
+dendogram() #TO DECIDE CLUSTER NUMBER FOR AGNES, ONLY RAN FOR ONCE
+"""
+
+timer1 = time.perf_counter()
 agnes()
-timer1 = time.perf_counter()                     #timer starts
+timer2 = time.perf_counter()
+print(f"\nAGNES done in {timer2 - timer1:0.4f} seconds")
+
+
+timer1 = time.perf_counter()
 kMeans()
-timer2 = time.perf_counter()                     #timer ends
+timer2 = time.perf_counter()
 print(f"\nK Means done in {timer2 - timer1:0.4f} seconds")
