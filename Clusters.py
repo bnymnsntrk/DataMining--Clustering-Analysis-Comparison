c8928971e09868734be2225d4a3c76a2b343cd25
bnymnsntrk <bnymnsntrk@gmail.com>
2020-05-24 17:49:15 +0300

    Finalized

--- Clusters.py
+++ Clusters.py
@@ -18,20 +18,22 @@ Kemal Barbaros - 150116070
 
 
 data = pd.read_csv("data.csv")              # read data
-X = data.iloc[:, [8]]
 
 """print(data.isna().sum())  # check if there are missing values"""
 data = data.interpolate(method='linear', limit_direction='forward')     # fill missing values with linear forwarding
-data = data.drop(['date'], axis=1)      # we don't need date
+data = data.drop(['date'], axis=1)                                  # we don't need these columns
+data = data.drop(['percent_change_price'], axis=1)
+data = data.drop(['percent_change_volume_over_last_wk'], axis=1)
+data = data.drop(['previous_weeks_volume'], axis=1)
 
-labelEncoder = LabelEncoder()           # converting 'stock' to numerical value
-labelEncoder.fit(data['stock'])
-data['stock'] = labelEncoder.transform(data['stock'])
+stocknum = LabelEncoder()           # converting 'stock' to numerical value
+stocknum.fit(data['stock'])
+data['stock'] = stocknum.transform(data['stock'])
 
 data = data.iloc[1:]            # dropping first row because it can not be interpolated
 
-scaler = StandardScaler()                                       # scaling data
-data_scaled = scaler.fit_transform(data)
+scale = StandardScaler()                                       # scaling data
+data_scaled = scale.fit_transform(data)
 
 data_normalized = normalize(data_scaled)                        # normalizing data
 data_normalized = pd.DataFrame(data_normalized, columns=data.columns)
@@ -44,13 +46,55 @@ data_2d = pca.fit_transform(data_normalized)
 data_2d = pd.DataFrame(data_2d)
 data_2d.columns = ['x', 'y']
 
-# data_2d.to_csv(r'2d.csv', index=False)
+# data_2d.to_csv(r'2d.csv', index=False)        check export
+
 
 def agnes():
-    cluster = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='ward')
-    cluster.fit_predict(data_2d)
-    plt.figure(figsize=(10, 7))
-    plt.scatter(data_2d['x'], data_2d['y'], c=cluster.labels_)
+    timer1 = time.perf_counter()        # timer begins
+
+    agnes = AgglomerativeClustering(n_clusters=4, affinity='euclidean', linkage='ward')     # clustering
+    agnes.fit_predict(data_2d)
+    plt.figure(figsize=(10, 7))                                 # graphic output
+    plt.scatter(data_2d['x'], data_2d['y'], c=agnes.labels_)
+    plt.xlabel('')
+    plt.ylabel('Percent Change of Price')
+
+    timer2 = time.perf_counter()        # timer ends
+    print(f"\nAGNES done in {timer2 - timer1:0.4f} seconds")
+    plt.show()
+
+
+def k_means():
+    timer1 = time.perf_counter()        # timer begins
+
+    kmeans = KMeans(n_clusters=4, init='k-means++', max_iter=300, n_init=10, random_state=0)    # clustering
+    kmeans.fit_predict(data_2d)
+
+    plt.figure(figsize=(10, 7))         # graphic output
+    plt.scatter(data_2d['x'], data_2d['y'], c=kmeans.labels_)
+    plt.xlabel('')
+    plt.ylabel('Percent Change of Price')
+
+    timer2 = time.perf_counter()        # timer ends
+    print(f"\nK Means done in {timer2 - timer1:0.4f} seconds")
+
+    plt.show()
+
+
+def dbscan():
+    timer1 = time.perf_counter()        # timer begins
+
+    dbs = DBSCAN(eps=0.1, min_samples=5)        # clustering
+    dbs.fit(data_2d)
+
+    plt.figure(figsize=(10, 7))         # graphic output
+    plt.scatter(data_2d['x'], data_2d['y'], c=dbs.labels_)
+    plt.xlabel('')
+    plt.ylabel('Percent Change of Price')
+
+    timer2 = time.perf_counter()        # timer ends
+    print(f"\nDBSCAN done in {timer2 - timer1:0.4f} seconds")
+
     plt.show()
 
 
@@ -64,36 +108,15 @@ def dendogram():
 
 def elbow():
     wcss = []
-    for i in range(1,11):
+    for i in range(1,10):       # try clusters from 1 to 10
         kmeans=KMeans(n_clusters= i, init='k-means++', max_iter=300, n_init=10, random_state=0)
         kmeans.fit(data_scaled)
         wcss.append(kmeans.inertia_)
-    plt.plot(range(1,11),wcss)
+    plt.plot(range(1,10),wcss)
     plt.title('Elbow Method Graphic')
     plt.xlabel('Clusters')
     plt.ylabel('wcss')
     plt.show()
-
-
-def kMeans():
-    kmeans = KMeans(n_clusters=4, init='k-means++', max_iter=300, n_init=10, random_state=0)
-    kmeans.fit_predict(data_2d)
-
-    plt.figure(figsize=(10, 7))
-    plt.scatter(data_2d['x'], data_2d['y'], c=kmeans.labels_)
-    plt.show()
-
-    """plt.scatter(X[kmeans == 0, 0], X[kmeans == 0, 1], s=100, c='red', label='Careful')
-    plt.scatter(X[kmeans == 1, 0], X[kmeans == 1, 1], s=100, c='blue', label='Standard')
-    plt.scatter(X[kmeans == 2, 0], X[kmeans == 2, 1], s=100, c='green', label='Target')
-    plt.scatter(X[kmeans == 3, 0], X[kmeans == 3, 1], s=100, c='cyan', label='Careless')
-    # apply centroid for all cluster
-    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='Centroids')
-    plt.title('Cluster of Client')
-    plt.xlabel('Annual income (k$)')
-    plt.ylabel('Spending Score(1-100)')
-    plt.legend()
-    plt.show()"""
 
 
 """
@@ -101,13 +124,8 @@ elbow() #ELBOW METHOD FUNCTION, TAKES TOO MUCH TIME TO RUN, ONLY RAN FOR ONCE
 dendogram() #TO DECIDE CLUSTER NUMBER FOR AGNES, ONLY RAN FOR ONCE
 """
 
-timer1 = time.perf_counter()
 agnes()
-timer2 = time.perf_counter()
-print(f"\nAGNES done in {timer2 - timer1:0.4f} seconds")
 
+k_means()
 
-timer1 = time.perf_counter()
-kMeans()
-timer2 = time.perf_counter()
-print(f"\nK Means done in {timer2 - timer1:0.4f} seconds")
+dbscan()
